5da3552a7fce58698de044062a797f205ce8f5dd
RtyXmd <rongtianyang@inin88.com>
2022-01-14 17:12:43 +0800

    add post log

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,31 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/getTestServer", function(req, res, next) {
+router.get("/getTestServer", function (req, res, next) {
   // res.render('index', { title: 'Express' });
   var result = {
     //返回数据与格式
     code: 200,
-    msg: "测试成功"
+    msg: "测试成功",
+  };
+  res.json(result); //响应返回json数据
+});
+
+router.post("/logs", function (req, res, next) {
+  let body = "";
+  req.on("data", function (chunk) {
+    body += chunk;
+    console.log("chunk:", chunk);
+  });
+  req.on("end", () => {
+    console.log("---------------------body---------------------");
+    console.log(body);
+    res.end();
+  });
+  var result = {
+    //返回数据与格式
+    code: 200,
+    msg: "测试成功",
   };
   res.json(result); //响应返回json数据
 });
